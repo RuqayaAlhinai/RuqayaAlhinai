de4a460d30b77289c40b20ff40d3b0356fe248d9
RuqayaAlhinai <138997183+RuqayaAlhinai@users.noreply.github.com>
2023-08-08 07:09:55 +0400

    Add files via upload

---
+++ weather-journal-app/server.js
@@ -0,0 +1,34 @@
+
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+projectData = {};
+app.use(express.static("website"));
+const getAll = (req, res) => res.status(200).send(projectData);
+app.get("/all", getAll);
+
+
+const postData = (req, res) => {
+    projectData = req.body;
+    console.log(projectData);
+    res.status(200).send(projectData);
+  }
+
+app.post("/add", postData);
+
+const port = 4000;
+const hostname = "127.0.0.1";
+
+
+const listening = () =>
+console.log(`Server running at http://${hostname}:${port}/`);
+
+app.listen(port, listening);
---
+++ weather-journal-app/website/app.js
@@ -0,0 +1,105 @@
+// Creating a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.toDateString();
+
+
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+
+const apiKey = ",&appid=f8a8572d48a545a0b83fe832899d2384&units=metric";
+
+// the URL of the server to post data
+const server = "http://127.0.0.1:4000";
+
+// showing the error to the user
+const error = document.getElementById("error");
+
+
+const generateData = () => { 
+  //get value after click on the button
+  const zip = document.getElementById("zip").value;
+  const feelings = document.getElementById("feelings").value;
+
+  // getWeatherData return promise
+  getWeatherData(zip).then((data) => {
+    //making sure from the received data to execute rest of the steps
+    if (data) {
+      const {
+        main: { temp },
+        name: city,
+   
+      } = data;
+
+      const info = {
+        newDate,
+        city,
+        temp: Math.round(temp), // to get integer number
+    
+        feelings,
+      };
+
+      postData(server + "/add", info);
+
+      updatingUI();
+      document.getElementById('entry').style.opacity = 1;
+    }
+  });
+};
+
+// Event listener to add function to existing HTML DOM element
+// Function called by event listener
+document.getElementById("generate").addEventListener("click", generateData);
+
+//Function to GET Web API Data
+const getWeatherData = async (zip) => {
+  try {
+    const res = await fetch(baseURL + zip + apiKey);
+    const data = await res.json();
+
+    if (data.cod != 200) {
+      // display the error message on UI
+      error.innerHTML = data.message;
+      setTimeout(_=> error.innerHTML = '', 2000)
+      throw `${data.message}`;
+    }
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Function to POST data
+const postData = async (url = "", info = {}) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(info),
+  });
+
+  try {
+    const newData = await res.json();
+    console.log(`You just saved`, newData);
+    return newData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Function to GET Project Data
+// and updating UI by this data
+const updatingUI = async () => {
+  const res = await fetch(server + "/all");
+  try {
+    const savedData = await res.json();
+
+    document.getElementById("date").innerHTML = savedData.newDate;
+   // document.getElementById("city").innerHTML = savedData.city;
+    document.getElementById("temp").innerHTML = savedData.temp + '&degC';
+   // document.getElementById("description").innerHTML = savedData.description;
+    document.getElementById("content").innerHTML = savedData.feelings;
+  } catch (error) {
+    console.log(error);
+  }
+};
